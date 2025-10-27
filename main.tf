8faf6ae14f04e95e48a0db449e8491c21b8d8369
Kofi <112496906+kamoah233@users.noreply.github.com>
2025-10-27 00:15:59 -0400

    vpc

--- main.tf
+++ main.tf
@@ -24,6 +24,9 @@ resource "aws_instance" "my_ec2_instance" {
 
 resource "aws_vpc" "my_vpc" {
         cidr_block = "10.0.0.0/16 "
+        tags = {
+            Name = "MyFirstTerraformVPC"
+        }
   }
 
     
